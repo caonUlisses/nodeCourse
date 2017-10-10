c6cf5558a51a20cc219cd6c0fee605b2a0f4816f
Ulisses Caon <ulissescaon@gmail.com>
2017-10-10 20:34:18 -0300

    Add first validation rules

--- module5/server/server.js
+++ module5/server/server.js
@@ -5,29 +5,24 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type     : String,
+    required : true,
+    minlength: 1,
+    trim     : true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type   : Number,
+    default: null
   }
 });
 
-// let newTodo = new Todo({
-//   text: 'Eat dinner',
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-
 let otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
+  text       : 'Feed the cat',
+  completed  : true,
   completedAt: 123
 });
 
@@ -35,4 +30,4 @@ otherTodo.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }), (e) => {
   console.log('Unable to save todo', e);
-}
+};
