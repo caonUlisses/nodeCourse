38ffc051524489680251b3e80d3275fa20eaacae
Ulisses Caon <ulissescaon@gmail.com>
2017-10-11 22:39:09 -0300

    Remove playground test

--- module5/playground/mongodb-find.js
+++ module5/playground/mongodb-find.js
@@ -7,21 +7,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
      
      console.log('Connected to MongoDB server');
 
-    //  db.collection('Todos').find({
-    //      _id: new ObjectID('59cfe3e3b23614460ad2013f')
-    //     }).toArray().then((docs) => {
-    //     console.log('Todos: ');
-    //     console.log(JSON.stringify(docs, undefined, 2));
-    //  }, (err) => {
-    //     console.log('Unable to fetch todos', err);
-    //  });
-
-    // db.collection('Todos').find().count().then((count) => {
-    //    console.log(`Todos count: ${count}`);
-    // }, (err) => {
-    //    console.log('Unable to fetch todos', err);
-    // });
-
     db.collection('Users').find({'name': 'Ulisses'}).toArray().then((docs) => {
         console.log(JSON.stringify(docs, undefined, 2));
     });
