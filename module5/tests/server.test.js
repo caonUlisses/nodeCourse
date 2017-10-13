4886419c4d7d628bc29d6457e89469adbb2f7be7
Ulisses Caon <ulissescaon@gmail.com>
2017-10-13 16:51:17 -0300

    Add second test

--- module5/tests/server.test.js
+++ module5/tests/server.test.js
@@ -167,9 +167,21 @@ describe('PATCH /todos/:id', () => {
     });
 
     it('should clear completedAt when todo is not completed', (done) => {
-        let hexId = todos[0]._id.toHexString();
+        let hexId = todos[1]._id.toHexString();
+        let text  = 'Testing second todo';
 
         request(app)
-            .patch()
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done);
     });
 });
