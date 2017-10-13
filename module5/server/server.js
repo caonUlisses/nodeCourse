ffb3c2534436ba66d0dabd579117cfd594804ffb
Ulisses Caon <ulissescaon@gmail.com>
2017-10-12 23:17:02 -0300

    Add patch todos

--- module5/server/server.js
+++ module5/server/server.js
@@ -1,5 +1,6 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const _          = require('lodash');
 
 const {ObjectId}   = require('mongodb');
 const { mongoose } = require('./db/mongoose');
@@ -60,6 +61,32 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     return res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id   = req.params.id;
+  let body = _.pick(rq.body, ['text', 'completed']);
+
+  if(!ObjectId.isValid(id)) {
+    res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed   = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if(!todo) {
+      return res.status(400).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
