6de6e9a4a8dd6aaef040642dc2ceb650176dce6e
Ulisses Caon <ulissescaon@gmail.com>
2017-10-12 21:50:47 -0300

    Add GET /todos/:id

---
+++ module5/playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectId} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo}     = require('./../server/models/todo');
+const {User}     = require('./../server/models/user');
+
+let id = '59dfed8fc95512c948d28bda';
+
+if (!ObjectId.isValid(id)) {
+    console.log('ID not found');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos)
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo)
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id', todo)
+// }).catch((e) => console.log(e));
--- module5/server/server.js
+++ module5/server/server.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectId}   = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo }     = require('./models/todo');
 const { User }     = require('./models/user');
@@ -26,6 +27,26 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  
+  if(!ObjectId.isValid(id)) {
+    res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send(); 
+    }
+
+    return res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
 });
 
 app.listen(3000, () => {
