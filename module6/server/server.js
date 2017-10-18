ecd1c96a6302517e98942da6de3b1c328755892d
Ulisses Caon <ulissescaon@gmail.com>
2017-10-18 09:44:16 -0200

    Greet new users and alert others

--- module6/server/server.js
+++ module6/server/server.js
@@ -15,12 +15,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+            from: "Admin",
+            text: "Welcome to the chat app"
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'A new user has joined'
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createdMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
         });
     });
 
